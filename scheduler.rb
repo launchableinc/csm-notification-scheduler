fce19a1ce931700ff3f8abffbc1092604babe512
Yoshiori SHOJI <yshoji@launchableinc.com>
2021-10-27 20:26:17 +0900

    Create and add ical file

--- scheduler.rb
+++ scheduler.rb
@@ -1,9 +1,11 @@
 require "json"
 require "yaml"
 require "date"
+require "icalendar"
 
 SCHEDULE_FILE = File.join(__dir__, "schedule.json")
 MEMBER_FILE = File.join(__dir__, "member.yml")
+ICAL_FILE = File.join(__dir__, "schedule.ics")
 
 schedules = JSON.load(File.open(SCHEDULE_FILE))
 members = YAML.load_file(MEMBER_FILE)
@@ -19,7 +21,15 @@ schedules.unshift(
   "end" => next_cutoverday.iso8601,
   "assignee" => next_assignee
 )
-File.write(SCHEDULE_FILE ,JSON.pretty_generate(schedules))
-today = Date.today
-current_index = schedules.index {|schedule| (Date.parse(schedule["start"])..Date.parse(schedule["end"])).cover? today }
-p current_index
+File.write(SCHEDULE_FILE, JSON.pretty_generate(schedules))
+
+cal = Icalendar::Calendar.new
+cal.append_custom_property("X-WR-CALNAME;VALUE=TEXT", "CSM Notification checker")
+schedules.each do |s|
+  cal.event do |e|
+    e.dtstart     = Icalendar::Values::Date.new(Date.parse(s["start"]))
+    e.dtend       = Icalendar::Values::Date.new(Date.parse(s["end"]) + 1)
+    e.summary     = "#{s["assignee"]} 👈 CSM"
+  end
+end
+File.write(ICAL_FILE, cal.to_ical)
